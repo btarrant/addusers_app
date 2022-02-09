9803bde66af97ed01dd10c6c84a6e673a007f626
Brandon Tarrant <brandontarrant04@gmail.com>
2022-02-08 23:05:18 -0500

    Created Users class to retrieve data from SQL.

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, redirect
 
+from users import User
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return redirect('/users')
+
+
+@app.route('/users')
+def users():
+    return render_template("users.html", users=User.getall())
 
 
 if __name__ == "__main__":
--- users.py
+++ users.py
@@ -1,31 +1,31 @@
 from mysqlconnection import connectToMySQL
 
 
-class Friend:
+class User:
 
     def __init__(self, data):
 
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
-        self.occupation = data['occupation']
+        self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
     @classmethod
     def get_all(cls):
-        query = "SELECT * FROM friends;"
+        query = "SELECT * FROM users;"
 
         results = connectToMySQL('users_schema').query_db(query)
 
-        friends = []
+        users = []
         # Iterate over the db results and create instances of friends with cls.
-        for friend in results:
-            friends.append(cls(friend))
-        return friends
+        for u in results:
+            users.append(cls(u))
+        return users
 
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
         # data is a dictionary that will be passed into the save method from server.py
         return connectToMySQL('users_schema').query_db(query, data)
