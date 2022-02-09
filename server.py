74e52c94855b82cdb9eb0de5b001b6a68b117090
Brandon Tarrant <brandontarrant04@gmail.com>
2022-02-08 22:26:40 -0500

    Added root route.

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect('/users')
 
 
 if __name__ == "__main__":
